c6ba72f2a68b7d186157f606a87e71d0b98ad0d4
Jonn Mostovoy <amarr.industrial@gmail.com>
2013-09-02 01:24:27 +0200

    Hide exposed functions; refactor tests.

--- 1-building-abstractions-with-procedures/1.7.lisp
+++ 1-building-abstractions-with-procedures/1.7.lisp
@@ -1,47 +1,50 @@
 (load "sqrt-newton-example.lisp")
 
-(define (test_do x)
-  (define result (sqrt-newton x))
-  (define mistake (abs (- (square result) x)))
-  (or (and (< mistake precision) ; we either meet requirements
-        (< mistake (* x precision))) ; even for small numbers
-      (= 0 (- (sqrt x) result)))) ; or we couldn't do better
+(define (test)
+  (define (test_do x)
+    (define result (sqrt-newton x))
+    (define mistake (abs (- (square result) x)))
+    (or (and (< mistake precision) ; we either meet requirements
+          (< mistake (* x precision))) ; even for small numbers
+        (= 0 (- (sqrt x) result)))) ; or we couldn't do better
 
-(define (test_small x)
-  (if (= x 0)
-    (= 0 0)
-    (and (test_do (/ 1.0 (expt 10 x))) 
-         (test_small (- x 1)))))
+  (define (test_small x)
+    (if (= x 0)
+      (= 0 0)
+      (and (test_do (/ 1.0 (expt 10 x))) 
+           (test_small (- x 1)))))
 
-(define (test_normal x) (and
-  (if (= x 0)
-    (= 0 0)
-    (and (test_do x)
-         (test_normal (- x 1)))))
+  (define (test_normal x) (and
+    (if (= x 0)
+      (= 0 0)
+      (and (test_do x)
+           (test_normal (- x 1)))))
+  )
+
+  (define (test_big x)
+    (if (= x 0)
+      (= 0 0)
+      (and (test_do (expt 10 x))
+           (test_big (- x 1)))))
+
+  (and
+    (display "Running small test...\n")
+    (or 
+      (test_small 4)
+      (and (display "FAIL (small test)!\n")
+      #f))
+
+    (display "Running normal test...\n")
+    (or
+      (test_normal 10)
+      (and (display "FAIL (normal test)!\n")
+      #f))
+
+    (display "Running big test...\n")
+    (or 
+      (test_big 13)
+      (and (display "FAIL (big test)!\n")
+      #f)))
 )
 
-(define (test_big x)
-  (if (= x 0)
-    (= 0 0)
-    (and (test_do (expt 10 x))
-         (test_big (- x 1)))))
-
-(define test (and
-  (display "Running small test...\n")
-  (or 
-    (test_small 4)
-    (and (display "FAIL (small test)!\n")
-    #f))
-
-  (display "Running normal test...\n")
-  (or
-    (test_normal 10)
-    (and (display "FAIL (normal test)!\n")
-    #f))
-
-  (display "Running big test...\n")
-  (or 
-    (test_big 13)
-    (and (display "FAIL (big test)!\n")
-    #f))
-))
+(test)
--- 1-building-abstractions-with-procedures/sqrt-newton-example.lisp
+++ 1-building-abstractions-with-procedures/sqrt-newton-example.lisp
@@ -1,26 +1,23 @@
 (define (sqrt-newton x)
+  (define (sqrt-iter guess x)
+    (if (good-enough? guess x)
+      guess
+      (sqrt-iter (improve guess x)
+                  x)))
+  (define (improve guess x)
+    (average guess (/ x guess)))
+  (define (average x y)
+    (/ (+ x y) 2))
+  (define min_delta 
+    (square precision))
+  (define (good-enough? guess x) (and 
+    (display x)
+    (display ": Checking if ")
+    (display guess)
+    (display " is good enough\n")
+    (or
+      (< (abs (- (improve guess x) guess)) min_delta))
+  ))
   (sqrt-iter 1.0 x))
 
-(define (sqrt-iter guess x)
-  (if (good-enough? guess x)
-    guess
-    (sqrt-iter (improve guess x)
-                x)))
-
-(define (improve guess x)
-  (average guess (/ x guess)))
-
-(define (average x y)
-  (/ (+ x y) 2))
-
-(define (good-enough? guess x) (and 
-  (display x)
-  (display ": Checking if ")
-  (display guess)
-  (display " is good enough\n")
-  (or
-    (< (abs (- (improve guess x) guess)) min_delta))
-))
-
-(define min_delta (square precision))
 (define precision 0.00001)
