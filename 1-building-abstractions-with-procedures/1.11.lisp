c787e22bb298482af1216aa5fd05588620816f3d
Jonn Mostovoy <amarr.industrial@gmail.com>
2013-09-24 06:36:51 +0200

    Hide private fns; fix f-bad.

--- 1-building-abstractions-with-procedures/1.11.lisp
+++ 1-building-abstractions-with-procedures/1.11.lisp
@@ -1,16 +1,18 @@
 (define (f-bad n)
   (cond ((< n 3) n)
-        (else (+ (f (- n 1)) 
-                 (* 2 (f (- n 2))) 
-                 (* 3 (f (- n 3))) )) ))
+        (else (+ (f-bad (- n 1)) 
+                 (* 2 (f-bad (- n 2))) 
+                 (* 3 (f-bad (- n 3))) )) ))
 
 (define (f-good n)
+  (define (y a b c)
+    (+ a (* 2 b) (* 3 c)) )
+
+  (define (f-iter i n a b c)
+    (define r (y a b c))
+    (cond ((= i n) r)
+          (else (f-iter (+ 1 i) n r a b)) ))
+
   (cond ((< n 3) n)
-        (else (f-iter 3 n 2 1 0)) ))
-
-(define (f-iter i n a b c)
-  (cond ((= i n) r)
-        (else (f-iter (+ 1 i) n r a b)) ))
-
-(define (y a b c)
-  (+ a (* 2 b) (* 3 c)) )
+        (else (f-iter 3 n 2 1 0)) )
+)
