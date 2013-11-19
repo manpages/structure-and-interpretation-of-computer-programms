7ef7ed44fcbc616dcb08bb9cab412a80021418f4
Jonn Mosotovy <amarr.industrial@gmail.com>
2013-11-19 10:32:17 +0100

    Make fast-expt iterative.

--- 1-building-abstractions-with-procedures/1.16.lisp
+++ 1-building-abstractions-with-procedures/1.16.lisp
@@ -1,7 +1,13 @@
 (define (fast-expt b n)
-  (cond ((= n 0) 1)
-    ((even? n) (square (fast-expt b (/ n 2))))
-    (else (* b (fast-expt b (- n 1))))) )
+  (fast-expt-iter b n 1))
+
+(define (fast-expt-iter b n a)
+  (cond ((= n 0) a)
+        ; <3 popoffka
+        (else (fast-expt-iter (square b) 
+                              (quotient n 2) 
+                              (cond ((even? n) a)
+                                    (else (* a b)) ))) ))
 
 (define (even? n)
   (= (remainder n 2) 0) )
